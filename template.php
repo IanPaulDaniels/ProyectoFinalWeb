1a140ece1ad82d2d519a824b5f25acd79a4672b4
Ndiazfuentes <ndiaz.fuentes@live.com>
2014-05-13 19:43:01 -0400

    Template V1 Complete

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Pl&aacute;tano Solutions | [PAGE TITLE]</title>
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+
+<body>
+
+<!-- CONTAINER -->
+<div id="container">
+  <!-- HEADER -->
+<?php include("php/header.inc"); ?>
+  
+  <div class="content-area">
+  
+  <!-- MENU -->
+<?php include("php/menu.inc"); ?>
+  
+  <!-- CONTENT GOES HERE -->
+  <h3>Recent Actions</h3>
+  <p>Stuff goes here</p>
+  
+  </div>
+  <!-- FOOTER -->
+<?php include("php/footer.inc"); ?>
+
+</div>
+
+</body>
+</html>
